6df922d4c0aa737b502d0836080f320e3e769608
aleks8223 <aleks8223@hotmail.com>
2023-07-13 00:27:32 +0600

    Добавлено задача 58

---
+++ Zadacha58/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, 
+// которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+Console.Write("Введите количество строк первого массива: ");
+int rowsA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов первого массива: ");
+int columnsA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество строк второго массива: ");
+int rowsB = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов второго массива: ");
+int columnsB = int.Parse(Console.ReadLine()!);
+
+int[,] A = GetArray(rowsA, columnsA, 0, 10);
+int[,] B = GetArray(rowsB, columnsB, 0, 10);
+PrintArray(A);
+Console.WriteLine();
+PrintArray(B);
+Console.WriteLine();
+PrintArray(GetMultiplyMatrix(A,B));
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue +1);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]}");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] GetMultiplyMatrix(int[,] arrayA, int[,] arrayB)
+{
+    int[,] arrayC = new int[arrayA.GetLength(0), arrayB.GetLength(1)];
+    for (int i = 0; i < arrayA.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayB.GetLength(1); j++)
+        {
+            for (int k = 0; k < arrayA.GetLength(1); k++)
+            {
+                arrayC[i,j] += arrayA[i,k] * arrayB[k,j];
+            }
+        }
+    }
+    return arrayC;
+}
+if (columnsA != rowsB)
+{
+    Console.WriteLine("Такие матрицы умножать нельзя!");
+    return;
+}
